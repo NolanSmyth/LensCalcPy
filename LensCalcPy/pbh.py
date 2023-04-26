d5b17c6b8a8c820b2012f6051ffdd2ae12a8c90a
Nolan Smyth <nolanwsmyth@gmail.com>
2023-04-26 12:45:48 -0700

    FFP crossing time implemented

--- LensCalcPy/pbh.py
+++ LensCalcPy/pbh.py
@@ -6,8 +6,10 @@ __all__ = ['Pbh']
 # %% ../nbs/00_pbh.ipynb 3
 from .parameters import *
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import nquad
 
-# %% ../nbs/00_pbh.ipynb 4
+# %% ../nbs/00_pbh.ipynb 5
 class Pbh:
     """A class to represent a PBH population"""
 
@@ -15,11 +17,8 @@ class Pbh:
                 m_pbh: float, # PBH mass in solar masses
                 f_dm: float # PBH fraction of the DM density
                 ):
-        """Initialize the PBH population
-
-        Args:
-            m_pbh (float): PBH mass in solar masses
-            f_dm (float): PBH fraction of the DM density
+        """
+        Initialize the PBH population
         """
         self.m_pbh = m_pbh
         if f_dm < 0 or f_dm > 1:
@@ -30,7 +29,9 @@ class Pbh:
         return f"PBH population with m_pbh={self.m_pbh} and f_dm={self.f_dm}"
     __repr__ = __str__
 
-    def density(self, r: float) -> float:
+    def density(self, 
+                r: float # distance in kpc
+                ) -> float:
         """PBH density at a given distance from the Milky Way center
         Using an NFW profile
 
@@ -40,27 +41,64 @@ class Pbh:
         Returns:
             float: PBH density in Msol/kpc^-3
         """
-        return rhoc / ((r/rs)*1 + r/rs)**2 * self.f_dm
+        return rhoc / ((r/rs) * (1 + r/rs)**2) * self.f_dm
     
-    def mass_enclosed(self, r: float) -> float:
+    def mass_enclosed(self, 
+                      r: float, # distance from MW center in kpc
+                      ) -> float:
         """PBH mass enclosed within a given distance from the Milky Way center
-
-        Args:
-            r (float): distance in kpc
 
         Returns:
             float: PBH mass in Msol
         """
-        return 4*np.pi * rhoc * rs**3 * (np.log(r + r/rs) - (r/rs)/(1 + r/rs))
+        return 4*np.pi * rhoc * rs**3 * (np.log(1 + r/rs) - (r/rs)/(1 + r/rs))
     
-    def velocity_dispersion(self, r: float) -> float:
+    def velocity_dispersion(self, 
+                            d: float # distance from Sun in kpc
+                            ) -> float:
         """PBH velocity dispersion at a given distance from the Milky Way center
-
-        Args:
-            r (float): distance in kpc
-
         Returns:
             float: PBH velocity dispersion in km/s
         """
+        r = self.dist_mw(d)
         return np.sqrt(G * self.mass_enclosed(r) / r) 
     
+    def velocity_radial(self,
+                        d: float, # distance from the Sun
+                        umin: float, # minimum impact parameter
+                        t: float # crossing time
+                        ) -> float:
+        """
+        PBH radial velocity at a given distance from the Sun
+        """
+        return 2*self.einstein_rad(d) * (ut**2 - umin**2)**(1/2) / t * kpctokm
+
+    
+    def dist(self,
+             d: float # distance from the Sun
+             ) -> float:
+        return d * (1 - d/ds)
+
+    def einstein_rad(self, 
+                     d: float, # distance from the Sun
+                     ) -> float:
+        return np.real((4 * G * self.m_pbh * self.dist(d)/c**2)**(1/2))
+    
+    def dist_mw(self, d):
+        """returns the distance to the Milky Way center in kpc of a point a distance d to the Sun in kpc"""
+        return np.sqrt(d**2 + rEarth**2 - 2*d*rEarth*np.cos(np.radians(l))*np.cos(np.radians(b)))
+    
+    def differential_rate_integrand(self, umin, d, t):
+        r = self.dist_mw(d)
+        return (1 / (ut**2 - umin**2)**0.5 *
+                self.density(r) / (self.m_pbh * self.velocity_dispersion(d)**2) *
+                self.velocity_radial(d, umin, t * htosec)**4 * (htosec / kpctokm)**2 *
+                np.exp(-(self.velocity_radial(d, umin, t * htosec)**2 / self.velocity_dispersion(d)**2)))
+    
+    def differential_rate(self, t):
+        umin_bounds = [0, ut]
+        d_bounds = [0, rEarth]
+
+        result, error = nquad(self.differential_rate_integrand, [umin_bounds, d_bounds], args=[t])
+
+        return result
