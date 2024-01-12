686d1e38d96c1971548d20adc616fac8c2f640f0
Duncan Wood <duncan@ucsc.edu>
2024-01-11 17:50:49 -0800

    remove numba from ut_func_new

--- LensCalcPy/utils.py
+++ LensCalcPy/utils.py
@@ -286,7 +286,7 @@ A_threshs = np.linspace(A_thresh_min, A_thresh_max, 20)
 
 
 # %% ../nbs/04_utils.ipynb 15
-@njit
+# @njit
 def ut_func_new(rho, A_thresh):
     # rho = np.asarray(rho) 
     rho = np.atleast_1d(rho) # Ensure rho is at least 1-dimensional
